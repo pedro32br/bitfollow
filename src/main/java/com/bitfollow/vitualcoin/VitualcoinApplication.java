03f1ff1f66eef607bcde1ed9b1ddee8ab64f0152
pedro.braga <pedrobragaom@gmail.com>
2019-10-10 11:17:50 -0300

    Initial commit: Basic structure and simple shape classes

---
+++ src/main/java/com/bitfollow/vitualcoin/VitualcoinApplication.java
@@ -0,0 +1,13 @@
+package com.bitfollow.vitualcoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitualcoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitualcoinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bitfollow/vitualcoin/VitualcoinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bitfollow.vitualcoin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VitualcoinApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
